48e9e9c04dc1264f589e126b7bd61ed30507b62b
ZodiaCC <53448802+TheZodiaCC@users.noreply.github.com>
2020-02-11 12:39:04 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Bubble_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int[] tab = new int[20];
+            int tmp;
+            
+            for(int i=0; i<tab.Length; i++)
+            {
+                tab[i] = random.Next(0, 50);
+            }
+            
+            for(int i=0; i<tab.Length - 1; i++)
+            {
+                for(int j=0; j<tab.Length - 1; j++)
+                {
+                    if(tab[j]>tab[j + 1])
+                    {
+                        tmp = tab[j];
+                        tab[j] = tab[j + 1];
+                        tab[j + 1] = tmp;
+                    }
+                }
+            }
+
+            foreach(int numb in tab)
+            {
+                Console.WriteLine(numb);
+            }
+        }
+    }
+}
